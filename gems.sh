8c1f8121867ee75dd0d7017f2105c481ff4167fe
James Beldock <james@xecuteventures.com>
2025-09-13 11:15:34 -0700

    Add manual soft link for colorls

--- gems.sh
+++ gems.sh
@@ -1,2 +1,9 @@
 sudo gem install colorls
 
+# edit this path with the correct version number;
+# find the version number via `gem which colorls`
+# or `gem env` if that doesn't find it
+
+ln -s /opt/homebrew/lib/ruby/gems/3.4.0/bin/colorls /opt/hom
+ebrew/bin/colorls
+
