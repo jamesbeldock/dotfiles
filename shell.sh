f21bee99e8f5f1cf839e3f0c55d8ddf408aa1f90
James Beldock <j@j.air.2.lan>
2025-09-12 19:03:43 -0700

    Fix homebrew install location for zsh

--- shell.sh
+++ shell.sh
@@ -1,10 +1,10 @@
 # install the Homebrew version of zsh and permit it to be a default shell
-sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells && chsh -s /usr/local/bin/zsh"
+sudo sh -c "echo '/opt/homebrew/bin/zsh' >> /etc/shells && chsh -s /opt/homebrew/bin/zsh"
 
 # install Oh My Zsh and upgrade
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 omz update
 
-# fix permissions on /usr/local/share directories
-chmod g-w,o-w /usr/local/share/zsh
-chmod g-w,o-w /usr/local/share/zsh/site-functions
+# fix permissions on /opt/homebrew/share directories
+chmod g-w,o-w /opt/homebrew/bin/zsh
+# chmod g-w,o-w /opt/homebrew/bin/zsh/site-functions
