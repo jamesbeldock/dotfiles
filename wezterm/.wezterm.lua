9a33635cca514c5e818d5e4d1de1453b2e805fa9
James Beldock <james@xecuteventures.com>
2025-09-20 22:59:27 -0700

    fix tab display

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,13 +8,13 @@ config.initial_cols = 80
 config.initial_rows = 28
 
 config.font_size = 14
-config.color_scheme = 'BlulocoDark'
-config.font = wezterm.font_with_fallback {
-    'JetBrainsMono Nerd Font',
-    'JetBrainsMono Nerd Font Mono',
-    'FiraCode Nerd Font',
-}
-config.enable_tab_bar = falsew
+config.color_scheme = "BlulocoDark"
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	"JetBrainsMono Nerd Font Mono",
+	"FiraCode Nerd Font",
+})
+config.enable_tab_bar = false
 
 -- Finally, return the configuration to wezterm:
 return config
