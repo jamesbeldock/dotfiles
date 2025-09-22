81bd57515f1d27f3511ca61e6a934dffe03195b6
James Beldock <james@xecuteventures.com>
2025-09-21 21:15:24 -0700

    Let the default bash run scripts

--- osx-package-install.sh
+++ osx-package-install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
