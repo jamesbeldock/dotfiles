54e7fcb62330a9e372664732dc24eb7f133ed7fa
James Beldock <james@xecuteventures.com>
2025-09-30 17:41:23 -0700

    Fix neovim init order

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lazy").setup({
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+})
