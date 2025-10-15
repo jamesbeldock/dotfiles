f105bb6e0299718d5aef9eefa6787c6bd3e325a3
James Beldock <james@xecuteventures.com>
2025-10-14 19:59:35 -0700

    Add server, workstation, and iot modes to stow-packages

--- stow-packages.sh
+++ stow-packages.sh
@@ -1,16 +1,55 @@
-PACKAGE=(
-	"basic"
-	"config resources"
-	"git"
-	"iterm2"
-	"nvim"
-	"oh-my-zsh"
-	"starship"
-	"tmux"
-	"wezterm"
-	"zsh"
-)
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ -z "$1" ] ; then
+	echo "Usage: stow-packages.sh [--help|-h] server|workstation|iot"
+	echo "This script uses GNU Stow to symlink dotfiles from the stow-packages directory to the home directory."
+	echo "Each subdirectory in stow-packages represents a package of dotfiles to be managed."
+	echo "server, workstation, and iot are predefined sets of packages."
+	exit 0	
+elif [ "$1" = "workstation" ] ; then
+	MODE="workstation"
+	PACKAGE=(
+		"basic"
+		"config resources"
+		"fastfetch"
+		"git"
+		"iterm2"
+		"nvim"
+		"oh-my-zsh"
+		"starship"
+		"tmux"
+		"wezterm"
+		"zsh"
+	)	
+elif [ "$1" = "server" ] ; then
+	MODE="server"
+	PACKAGE=(
+		"basic"
+		"config resources"
+		"fastfetch"
+		"git"
+		"nvim"
+		"tmux"
+		"zsh"
+	)	
+elif [ "$1" = "iot" ] ; then
+	MODE="iot"
+	PACKAGE=(
+		"basic"
+		"config resources"
+		"fastfetch"
+		"nvim"
+		"tmux"
+		"zsh"
+	)	
+else
+	exit 1
+fi
+
+echo "Stowing packages in $MODE mode..."
 
 for package in "${PACKAGE[@]}"; do
+	echo "Stowing package: $package"
 	stow -v -t ~/ --dotfiles "$package"
 done
+echo "All packages stowed successfully."
+
+exit 0
